1c652bc833a1b2cb67177a65308d3f3c16aebb25
Victor Nguyen <vic.nguyen@gmail.com>
2015-05-21 23:43:32 +1000

    added mocha + chai to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
- 
+
 var browserify  = require('browserify');
 var watchify    = require('watchify');
 var gulp        = require('gulp');
@@ -8,14 +8,20 @@ var buffer      = require('vinyl-buffer');
 var uglify      = require('gulp-uglify');
 var sourcemaps  = require('gulp-sourcemaps');
 var gutil       = require('gulp-util');
- 
+var mocha       = require('gulp-mocha');
+
+// setup watchify, browserify
 var b = watchify(
     browserify({
         entries: './src/index.js',
         debug: true
     })
 );
- 
+b.on('update', bundle);
+b.on('update', runTests);
+b.on('log', gutil.log); 
+
+
 function bundle() {
     return b.bundle()
         .on('error', gutil.log.bind(gutil, 'Browserify Error'))
@@ -28,7 +34,12 @@ function bundle() {
         .pipe(gulp.dest('./dist/'));
 }
 
- 
+
+function runTests() {
+    return gulp.src('test/*.js', { read:false })
+        .pipe( mocha({ reporter: 'spec' }) );
+}
+
+// gulp tasks
 gulp.task('js', bundle);
-b.on('update', bundle);
-b.on('log', gutil.log);
+gulp.task('test', runTests);
