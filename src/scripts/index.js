f3f43fa2d3adf0564bfdf03b9d461d8c8e528139
Victor Nguyen <vic.nguyen@gmail.com>
2015-05-24 23:36:49 +1000

    added external example input data to load into frontend

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,18 +2,34 @@
 
 // entry point, will handle UI for inputting commands and executing program
 
-var Simulation = require('./simulation');
+var Simulation   = require('./simulation'),
+    exampleData  = require('./example');
 
 
 var simulation = new Simulation({ x:5, y:5 });
 
-var textarea   = document.getElementById('commands'),
-    button     = document.getElementById('process'),
-    output     = document.getElementById('output');
+var textarea     = document.getElementById('commands'),
+    button       = document.getElementById('process'),
+    output       = document.getElementById('output'),
+    example_btns = document.querySelectorAll('.example');
 
-function doThemCommands() {
+function processInput() {
     simulation.process(textarea.value);
     output.textContent = simulation.getOutput();
 }
 
-button.addEventListener('click', doThemCommands);
+function loadExample() {
+    var key = this.getAttribute('data-example');
+    textarea.value = exampleData[key].input;
+}
+
+// bind process button
+button.addEventListener('click', processInput);
+
+// bind each example button
+for (var i = 0; i < example_btns.length; i++) {
+    example_btns[i].addEventListener('click', loadExample);
+}
+
+// load first example on load
+textarea.value = exampleData.a.input;
