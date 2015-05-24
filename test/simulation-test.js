79ccada9699aa7a965df1241e95d290a4cba8bd9
Victor Nguyen <vic.nguyen@gmail.com>
2015-05-24 14:37:42 +1000

    added more simulation tests

--- test/simulation-test.js
+++ test/simulation-test.js
@@ -30,15 +30,88 @@ describe('Simulation', function () {
     });
 
     describe('move()', function () {
+        it('should move to the correct position facing NORTH', function () {
+            simulation.place('PLACE 0,0,NORTH');
+            simulation.move();
+            expect( simulation.report() ).to.equal('0,1,NORTH');
+        });
+
+        it('should move to the correct position facing EAST', function () {
+            simulation.place('PLACE 0,0,EAST');
+            simulation.move()
+            expect( simulation.report() ).to.equal('1,0,EAST');
+        });
+
+        it('should move to the correct position facing SOUTH', function () {
+            simulation.place('PLACE 0,1,SOUTH');
+            simulation.move()
+            expect( simulation.report() ).to.equal('0,0,SOUTH');
+        });
+
+        it('should move to the correct position facing WEST', function () {
+            simulation.place('PLACE 1,0,WEST');
+            simulation.move()
+            expect( simulation.report() ).to.equal('0,0,WEST');
+        });
     });
 
     describe('left()', function () {
+        it('should rotate the robot from NORTH to WEST', function () {
+            simulation.place('PLACE 0,0,NORTH');
+            simulation.left();
+            expect( simulation.report() ).to.equal('0,0,WEST');
+        });
+
+        it('should rotate the robot from WEST to SOUTH', function () {
+            simulation.place('PLACE 0,0,WEST');
+            simulation.left();
+            expect( simulation.report() ).to.equal('0,0,SOUTH');
+        });
+
+        it('should rotate the robot from SOUTH to EAST', function () {
+            simulation.place('PLACE 0,0,SOUTH');
+            simulation.left();
+            expect( simulation.report() ).to.equal('0,0,EAST');
+        });
+
+        it('should rotate the robot from EAST to NORTH', function () {
+            simulation.place('PLACE 0,0,EAST');
+            simulation.left();
+            expect( simulation.report() ).to.equal('0,0,NORTH');
+        });
     });
 
     describe('right()', function () {
+        it('should rotate the robot from NORTH to EAST', function () {
+            simulation.place('PLACE 0,0,NORTH');
+            simulation.right();
+            expect( simulation.report() ).to.equal('0,0,EAST');
+        });
+
+        it('should rotate the robot from EAST to SOUTH', function () {
+            simulation.place('PLACE 0,0,EAST');
+            simulation.right();
+            expect( simulation.report() ).to.equal('0,0,SOUTH');
+        });
+
+        it('should rotate the robot from SOUTH to WEST', function () {
+            simulation.place('PLACE 0,0,SOUTH');
+            simulation.right();
+            expect( simulation.report() ).to.equal('0,0,WEST');
+        });
+
+        it('should rotate the robot from WEST to NORTH', function () {
+            simulation.place('PLACE 0,0,WEST');
+            simulation.right();
+            expect( simulation.report() ).to.equal('0,0,NORTH');
+        });
     });
 
     describe('report()', function () {
+        it('should return the correct report string after a PLACE', function () {
+            simulation.place('PLACE 1,1,NORTH');
+            expect( simulation.report() ).to.equal('1,1,NORTH');
+        });
     });
 
 });
